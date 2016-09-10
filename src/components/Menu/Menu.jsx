065ca3fc5339e99ca2e8fd514cac2ebc31682b55
Jeremy Marquis <jeremy@jeremymarquis.com>
2016-09-10 00:04:10 -0400

    menu footer

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -9,15 +9,23 @@ export default class Menu extends Component {
   render() {
     return (
       <div className="Menu">
+
         <header>
           My Junk
         </header>
+
         <ul>
           <li><NavLink to="/projects/2">Work Stuff</NavLink></li>
           <li><NavLink to="/projects/1">Finances</NavLink></li>
           <li><NavLink to="/projects/3">Blog post ideas to write about in the future</NavLink></li>
           <li><NavLink to="/projects/4">Gift ideas</NavLink></li>
         </ul>
+
+        <footer>
+          <figure></figure>
+          Jeremy Marquis
+        </footer>
+
       </div>
     )
   }
